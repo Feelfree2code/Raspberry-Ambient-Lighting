a027aa8867cad4cc393b60edf2b456d7ab06d670
Sevcan Alkan <sevdzhan.alkan@scalefocus.com>
2020-03-03 15:50:19 +0200

    #3 : SignalR removed

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using RPI.SignalR;
 
 namespace WebAPI
 {
@@ -27,7 +26,6 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +45,6 @@ namespace WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<CommunicationHub>("/communication");
             });
         }
     }
