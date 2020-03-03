a1c9fffdc7f4f7039b768485b5b803d9c2087676
Sevcan Alkan <sevdzhan.alkan@scalefocus.com>
2020-03-03 17:00:32 +0200

    #11 :  App layout created

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color } from './models/Color.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rpi';
+  
+  public colorOptions: Color[];
+  public currentColor: string;
+
+  constructor() {
+    this.colorOptions = [
+      { value: 1, text: 'Red, Purple, Blue' },
+      { value: 2, text: 'Green, Blue, Red' },
+      { value: 3, text: 'Pink, Purple, Blue' }
+    ];
+  
+    this.currentColor = '-';
+  }
+
+  public setColor(color: Color): void {
+    this.currentColor = color.text;
+  }
 }
